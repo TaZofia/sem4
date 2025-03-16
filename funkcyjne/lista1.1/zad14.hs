e45da94d349fe972bd545a6433ffd3e539d16215
zofia <ztkal2013@gmail.com>
2025-03-16 20:14:53 +0100

    fix

--- funkcyjne/lista1.1/zad14.hs
+++ funkcyjne/lista1.1/zad14.hs
@@ -2,7 +2,7 @@ divList:: Int -> [Int]
 divList n = [k | k <- [1..(n-1)], mod n k == 0]
 
 sumDiv:: Int -> Int
-sumDiv n = sum [k | k <- divList n]
+sumDiv n = sum (divList n)
 
 isPerfect:: Int -> Bool
 isPerfect n = sumDiv n == n
