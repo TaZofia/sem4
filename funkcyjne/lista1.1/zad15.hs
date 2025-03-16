05ce97508246884bdc72b5aa90a037378191ed27
zofia <ztkal2013@gmail.com>
2025-03-16 20:24:42 +0100

    not finished

---
+++ funkcyjne/lista1.1/zad15.hs
@@ -0,0 +1,15 @@
+divList:: Int -> [Int]
+divList n = [k | k <- [1..(n-1)], mod n k == 0]
+
+sumDiv:: Int -> Int
+sumDiv n = sum (divList n)
+
+areFriends:: Int -> Int -> Bool
+areFriends m n = (m /= n) && (sumDiv n == m) && (sumDiv m == n)
+
+pairs:: Int -> [(Int, Int)]
+pairs k = [(m, n) | m <- [1..k], let n = sumDiv m, m < n, sumDiv n == m ]
+
+main = do
+    let friendPairs = pairs 1000
+    print friendPairs
