5ccfe530e01819cac0eb415b49ba32592c6b2749
zofia <ztkal2013@gmail.com>
2025-03-27 17:23:25 +0100

    question to swaps

--- aisd/lista2/zad1/quickSort.cpp
+++ aisd/lista2/zad1/quickSort.cpp
@@ -21,19 +21,43 @@ void swap() {
     swaps++;
 }
 
-void quickSort(std::vector<int> A, int p, int q) {
-    if(p < q) {
-        q = partition(A, p, r);
+int partition(std::vector<int>& A, int p, int r) {
+    int x = A[r];       // Pivot
+    int i = p - 1;
+
+    for(int j = p; j < r; j++) {
+        compare();
+        if(A[j] <= x) {
+            i++;
+            int temp = A[j];
+            A[j] = A[i];
+            A[i] = temp;
+            swap();
+            if(i != j) {
+                std::cout << "Array after swap: ";
+                printArray(A);
+            }
+        }
+    }
+    int temp2 = A[i+1];
+    A[i+1] = A[r];
+    A[r] = temp2;
+    swap();
+    if(i+1 != r) {
+        std::cout << "Array after swap: ";
+        printArray(A);
+    }
+    return (i+1);
+}
+
+void quickSort(std::vector<int>& A, int p, int r) {
+
+    if(p < r) {
+        int q = partition(A, p, r);
         quickSort(A, p, q-1);
         quickSort(A, q+1, r);
     }
 }
-
-int partition() {
-
-}
-
-
 
 bool isSorted(const std::vector<int>& arr) {
     for (size_t i = 1; i < arr.size(); i++) {
@@ -60,14 +84,30 @@ int main() {
     int n = numbers[0];             // Number of elements
     numbers.erase(numbers.begin());
 
-    quickSort(numbers, 1, numbers.size())
-    
-    
+    std::vector<int> entranceArray = numbers;
+
+    if (numbers.size() < 40) {
+        std::cout << "Entrance array: ";
+        printArray(numbers);
+    }
+
+
+    quickSort(numbers, 0, numbers.size()-1);
+
+    if(numbers.size() < 40) {
+
+        std::cout << " Entrance array again: ";
+        printArray(entranceArray);
+
+        std::cout << "Array after QuickSort: ";
+        printArray(numbers);
+    }
+
     std::cout << "# of comparisons: " << comparisons << std::endl;
     std::cout << "# of swaps: " << swaps << std::endl;
     
     if (isSorted(numbers)) {
-        std::cout << "IS works" << std::endl;
+        std::cout << "QS works" << std::endl;
     } else {
         std::cout << "Error: Array not sorted" << std::endl;
     }
