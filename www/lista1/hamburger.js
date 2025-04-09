06f4d9e4aa76e526f44af99f32ed24c8d5df3c22
zofia <ztkal2013@gmail.com>
2025-04-09 16:34:00 +0200

    hamburger menu

---
+++ www/lista1/hamburger.js
@@ -0,0 +1,24 @@
+const fadeInMenu = document.querySelector(".fadeInMenu");
+const items = document.querySelectorAll(".item");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function menuFadeIn() {
+  if (fadeInMenu.classList.contains("showMenu")) {
+    fadeInMenu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    fadeInMenu.classList.add("showMenu");
+    closeIcon.style.display ="block";
+    menuIcon.style.display = "none";
+  }
+}
+hamburger.addEventListener("click", menuFadeIn);
+
+items.forEach(
+  function(menuItem) {
+    menuItem.addEventListener("click", menuFadeIn);
+  }
+)
