2d2e2152e0dca0e4f471e505461e48be402a9855
zofia <ztkal2013@gmail.com>
2025-04-15 00:01:34 +0200

    hangman to do canvas

---
+++ www/wisielec/hangman.js
@@ -0,0 +1,74 @@
+var sentence = "Gone with the wind"
+sentence = sentence.toUpperCase();
+
+var lengthSentence = sentence.length;
+var coveredSentence = "";
+
+/*covering sentence*/
+for(i = 0; i < lengthSentence; i++) {
+    if (sentence.charAt(i) == " ") coveredSentence = coveredSentence + " " 
+    else coveredSentence = coveredSentence + "-"
+}
+
+function write_sentence () {
+    document.getElementById("board").innerHTML = coveredSentence;
+}
+
+function write_letters () {
+
+    var alphabet = "";
+
+    for(i = 0; i <= 25; i ++) {
+        var singleLetter = String.fromCharCode(65 + i);
+        alphabet = alphabet + '<span class="letter" onclick="checkAndRewrite('+ (65 + i) + ')" id="' + (65 + i) + '">' + singleLetter + '</span>';
+        if((i + 1) % 7  == 0) alphabet = alphabet + '<span style="display: block; clear: both;"></span>';
+    }
+
+    document.getElementById("alphabet").innerHTML = alphabet;
+}
+
+function start () {
+
+    write_letters();
+    write_sentence();
+}
+
+window.onload = start;
+
+
+String.prototype.setChar = function(index, newChar) {
+
+    if(index > this.length - 1) {
+        return this.toString();
+    }
+    else {
+        return this.substr(0, index) + newChar + this.substr(index + 1);
+    }
+}
+
+
+
+function checkAndRewrite (nrAscii) {
+    
+    var correct = false;
+
+    for(i = 0; i < lengthSentence; i++) {  
+        if (sentence.charCodeAt(i) == nrAscii) {
+            coveredSentence = coveredSentence.setChar(i, String.fromCharCode(nrAscii));
+            correct = true;
+        }
+    }
+
+    /*span's id - ASCII of a letter*/
+    let element = document.getElementById(nrAscii);
+    element.style.background = "blue";
+    element.style.pointerEvents = "none"; 
+    element.style.cursor = "default"; 
+
+    if (!correct) {
+        
+    }
+
+    write_sentence();
+}
+
