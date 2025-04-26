7293fe2358a512a589eb791bb1547c9e263f7bf6
zofia <ztkal2013@gmail.com>
2025-04-26 20:59:21 +0200

    randomGenZ4.cpp done

--- aisd/lista3/zad4/randomGenZ4.cpp
+++ aisd/lista3/zad4/randomGenZ4.cpp
@@ -2,10 +2,11 @@
 #include <random>   // Mersenne Twister
 #include <chrono>
 #include <cmath>
+#include <cstring>
 
 int getIntFromArgs(int argc, char* argv[]) {
-    if (argc != 2) {
-        throw std::invalid_argument("Expected 1 argument");
+    if (argc != 3) {
+        throw std::invalid_argument("Expected 2 arguments");
     }
 
     try {
@@ -13,6 +14,13 @@ int getIntFromArgs(int argc, char* argv[]) {
     } catch (const std::invalid_argument&) {
         throw std::invalid_argument("Argument 1 must be a valid integer.");
     }
+}
+
+bool existsInArray(const std::vector<int>& arr, int value) {
+    for (int x : arr) {
+        if (x == value) return true;
+    }
+    return false;
 }
 
 int main(int argc, char* argv[]) {
@@ -23,15 +31,52 @@ int main(int argc, char* argv[]) {
         std::mt19937 mt(seed);  
         std::uniform_int_distribution<int> distData(1, 2*n-1);      // Range from 1 to 2n-1
 
-        std::cout << n << ", ";
+        std::cout << n << ", ";            // array size
 
-        std::cout << distData(mt) << ", ";        // random number which we are going to look for in binarySearch.cpp
+        std::vector<int> arr(n);
 
-        for(int i = 0; i < n - 1; i++) {
-            std::cout << distData(mt) << ", ";
+        for(int i = 0; i < n; i++) {        // add random numbers to array
+            arr[i] = distData(mt);
         }
-        std::cout << distData(mt);
-        
+
+        int indexOfElementToFind = 0;
+        int elementToFind = 0;
+
+        if (strcmp(argv[2], "start") == 0) {
+            int endIndex = std::ceil(n/5);
+            std::uniform_int_distribution<int> startData(0, endIndex-1);
+            indexOfElementToFind = startData(mt);
+            elementToFind = arr[indexOfElementToFind];
+
+        } else if (strcmp(argv[2], "end") == 0) {
+            int startIndex = std::floor((4*n)/5);
+            std::uniform_int_distribution<int> endData(startIndex, n-1);
+            indexOfElementToFind = endData(mt);
+            elementToFind = arr[indexOfElementToFind];
+
+        } else if (strcmp(argv[2], "middle") == 0) {
+            int startIndex = std::floor((2*n)/5);
+            int endIndex = std::floor((3*n)/5);
+            std::uniform_int_distribution<int> middleData(startIndex, endIndex);
+            indexOfElementToFind = middleData(mt);
+            elementToFind = arr[indexOfElementToFind];
+
+        } else if (strcmp(argv[2], "randomIn") == 0) {
+            std::uniform_int_distribution<int> inArray(0, n-1);
+            indexOfElementToFind = inArray(mt);
+            elementToFind = arr[indexOfElementToFind];
+        } else if (strcmp(argv[2], "randomOut") == 0) {
+            do {
+                elementToFind = distData(mt);
+            } while (existsInArray(arr, elementToFind));
+        }
+
+         std::cout << elementToFind << ", ";
+
+         for (int i = 0; i < n - 1; i++) {
+             std::cout << arr[i] << ", ";
+         }
+         std::cout << arr[n - 1] << std::endl;
 
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
