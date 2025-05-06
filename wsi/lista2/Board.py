e1bd33716e9d7773cb15cd27d23e71234c8cfe29
zofia <ztkal2013@gmail.com>
2025-05-06 19:55:43 +0200

    generating proper board done

--- wsi/lista2/Board.py
+++ wsi/lista2/Board.py
@@ -4,15 +4,64 @@ class Board:
     def __init__(self, size):
         self.size = size        # size - number of elements in row/column
         self.board = []
-        self.generate_perm()
+        self._generate_perm()
 
 
-    def generate_perm(self):
+    def _generate_perm(self):
         for i in range((self.size ** 2) - 1):
             self.board.append(i + 1)
 
         random.shuffle(self.board)
         self.board.append(0)
+
+
+    def _number_of_inversions(self):
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr, 0
+
+            mid = len(arr) // 2
+            left, inv_left = merge_sort(arr[:mid])
+            right, inv_right = merge_sort(arr[mid:])
+            merged, inv_split = merge_and_count(left, right)
+
+            return merged, inv_left + inv_right + inv_split
+
+        def merge_and_count(left, right):
+            merged = []
+            i = j = inv_count = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    merged.append(left[i])
+                    i += 1
+                else:
+                    merged.append(right[j])
+                    inv_count += len(left) - i  # other left elements are inversions
+                    j += 1
+
+            merged += left[i:]
+            merged += right[j:]
+            return merged, inv_count
+
+        # skip 0
+        filtered_board = [num for num in self.board if num != 0]
+        _, inv_count = merge_sort(filtered_board)
+        return inv_count
+
+
+    def _get_blank_row_number(self):
+        index_of_blank = self.board.index(0)
+        row = index_of_blank % self.size
+        return self.size - row              # because we numerate rows from 1 from the bottom
+
+
+    def can_be_solved(self):
+        if (self._number_of_inversions() + self._get_blank_row_number()) % 2 == 0:
+            return True
+        else:
+            return False
+
 
     def print_board(self):
         padding = len(str(len(self.board) - 1))
@@ -20,7 +69,6 @@ class Board:
             print(str(self.board[i]).zfill(padding), end=" ")
             if (i + 1) % self.size == 0:
                 print()
-
     @property
     def get_board(self):
         return self.board
@@ -28,12 +76,12 @@ class Board:
 
     def valid_moves(self):
 
-        index_of_empty = self.board.index(0)
+        index_of_blank = self.board.index(0)
 
-        left = index_of_empty - 1
-        right = index_of_empty + 1
-        up = index_of_empty - self.size
-        down = index_of_empty + self.size
+        left = index_of_blank - 1
+        right = index_of_blank + 1
+        up = index_of_blank - self.size
+        down = index_of_blank + self.size
 
         moves = []
 
@@ -47,10 +95,3 @@ class Board:
             moves.append(left)
 
         print(moves)
-
-
-board = Board(4)
-board.print_board()
-
-board.valid_moves()
-
---
+++ wsi/lista2/main.py
@@ -0,0 +1,28 @@
+from Board import Board
+
+def get_user_input():
+    while True:
+        try:
+            size = int(input("Enter board size: "))
+            if size >= 2:
+                return size
+            else:
+                print("Board size must be at least 2.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+
+def main():
+    size = get_user_input()
+
+    while True:
+        my_board = Board(size)
+        if my_board.can_be_solved():
+            break
+
+    my_board.print_board()
+    my_board.valid_moves()
+
+
+if __name__ == "__main__":
+    main()
