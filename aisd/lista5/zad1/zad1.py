78921a56780fd294a2fc51737c40dfcef754b8a1
zofia <ztkal2013@gmail.com>
2025-06-02 22:26:28 +0200

    zad1 done

--- aisd/lista5/zad1/zad1.py
+++ aisd/lista5/zad1/zad1.py
@@ -16,42 +16,50 @@ def generate_graph(n):
 def prim_mst(graph):
     n = len(graph.nodes)
     visited = [False] * n
-    min_heap = [(0, 0)]  # (weight, node)
+    min_heap = [(0, 0, -1)]  # (weight, node, parent)
     total_weight = 0
+    mst_edges = []
 
     while min_heap:
-        weight, u = heappop(min_heap)       # edge with the lowest weight
+        weight, u, parent = heappop(min_heap)
         if visited[u]:
             continue
         visited[u] = True
         total_weight += weight
+        if parent != -1:
+            mst_edges.append((parent, u, weight))
         for v in graph.neighbors(u):
             if not visited[v]:
-                heappush(min_heap, (graph[u][v]['weight'], v))
-    return total_weight
+                heappush(min_heap, (graph[u][v]['weight'], v, u))
+    return total_weight, mst_edges
 
 def kruskal_mst(graph):
     parent = {}
-    def find(x):            # helps to know if two vertices are already connected
+    def find(x):
         if parent[x] != x:
             parent[x] = find(parent[x])
         return parent[x]
+
     def union(x, y):
         rootX, rootY = find(x), find(y)
         if rootX != rootY:
-            parent[rootY] = rootX       # trying to connect two sets
-            return True         # connect
-        return False            # already connected
+            parent[rootY] = rootX
+            return True
+        return False
 
     for node in graph.nodes:
         parent[node] = node
 
     edges = sorted(graph.edges(data=True), key=lambda x: x[2]['weight'])
     total_weight = 0
+    mst_edges = []
+
     for u, v, data in edges:
         if union(u, v):
             total_weight += data['weight']
-    return total_weight
+            mst_edges.append((u, v, data['weight']))
+
+    return total_weight, mst_edges
 
 def run_experiment(n_min, n_max, step, rep):
     prim_times = []
@@ -64,14 +72,17 @@ def run_experiment(n_min, n_max, step, rep):
             G = generate_graph(n)
 
             t0 = time.perf_counter()
-            prim_mst(G)
+            prim_weight, prim_edges = prim_mst(G)
             t1 = time.perf_counter()
             prim_total += (t1 - t0)
 
             t0 = time.perf_counter()
-            kruskal_mst(G)
+            kruskal_weight, kruskal_edges = kruskal_mst(G)
             t1 = time.perf_counter()
             kruskal_total += (t1 - t0)
+
+            # Można ewentualnie sprawdzić zgodność wyników:
+            # assert abs(prim_weight - kruskal_weight) < 1e-6
 
         prim_times.append(prim_total / rep)
         kruskal_times.append(kruskal_total / rep)
@@ -88,10 +99,13 @@ def plot_results(sizes, prim_times, kruskal_times):
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig('./mst.png')
+    plt.savefig('./mst2.png')
 
+# Parametry eksperymentu
 n_min, n_max, step, rep = 10, 200, 10, 20
 
+# Uruchomienie eksperymentu
 sizes, prim_times, kruskal_times = run_experiment(n_min, n_max, step, rep)
 
+# Rysowanie wykresu
 plot_results(sizes, prim_times, kruskal_times)
